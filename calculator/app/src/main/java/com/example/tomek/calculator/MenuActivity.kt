c5ce3205d4bb3a557fad6eb5ed0c7a2e5ace4eff
TomaszKot11 <kottomasz98@gmail.com>
2019-03-09 09:57:16 +0100

    Added basic main menu layout

--- calculator/app/src/main/java/com/example/tomek/calculator/MenuActivity.kt
+++ calculator/app/src/main/java/com/example/tomek/calculator/MenuActivity.kt
@@ -1,12 +1,16 @@
 package com.example.tomek.calculator
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.menu_activity)
     }
 }
