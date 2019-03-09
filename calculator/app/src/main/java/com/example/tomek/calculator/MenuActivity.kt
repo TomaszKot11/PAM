8a80aed39d4fa6fb371bf2c0d6047b5cf144221f
TomaszKot11 <kottomasz98@gmail.com>
2019-03-09 08:32:44 +0100

    Initialized project

---
+++ calculator/app/src/main/java/com/example/tomek/calculator/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tomek.calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu_activity)
+    }
+}
