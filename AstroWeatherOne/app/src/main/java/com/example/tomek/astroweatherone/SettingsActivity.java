7b8ff9a268ccffe1464492a37abbaf97a1fbabb5
TomaszKot11 <kottomasz98@gmail.com>
2019-04-07 22:13:40 +0200

    Added settings apaters for spinners

--- AstroWeatherOne/app/src/main/java/com/example/tomek/astroweatherone/SettingsActivity.java
+++ AstroWeatherOne/app/src/main/java/com/example/tomek/astroweatherone/SettingsActivity.java
@@ -2,12 +2,31 @@ package com.example.tomek.astroweatherone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Spinner spinnerUnits;
+    private Spinner spinnerValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        spinnerValue = findViewById(R.id.time_value_spinner);
+        spinnerUnits = findViewById(R.id.time_unit_spinner);
+
+        assingCharAdaptersToSpinners(R.array.refreshing_numerical_values, spinnerValue);
+        assingCharAdaptersToSpinners(R.array.refreshing_units, spinnerUnits);
+    }
+
+    private void assingCharAdaptersToSpinners(int  string_array_identifier, Spinner spinner) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, string_array_identifier, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
