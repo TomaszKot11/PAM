3eb94cadc13015375191a1cffd6a0058c7a665a8
Tomasz Kot <kottomasz98@gmail.com>
2019-04-29 13:33:26 +0200

    Update

--- AstroWeatherTwo/app/src/main/java/com/example/university/astroweathertwo/mainActivity/fragments/AdditionalWeatherInformationFragment.java
+++ AstroWeatherTwo/app/src/main/java/com/example/university/astroweathertwo/mainActivity/fragments/AdditionalWeatherInformationFragment.java
@@ -10,14 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.university.astroweathertwo.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AdditionalWeatherInformationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AdditionalWeatherInformationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AdditionalWeatherInformationFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
