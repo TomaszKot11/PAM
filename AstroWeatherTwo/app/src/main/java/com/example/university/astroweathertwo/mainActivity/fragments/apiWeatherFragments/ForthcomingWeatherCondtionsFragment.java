598d18604be2dc936b80e2b8e49071f6a5870f5d
Tomasz Kot <kottomasz98@gmail.com>
2019-05-02 21:25:37 +0200

    Added dynamic tiles rendering in forthcommintg weather

--- AstroWeatherTwo/app/src/main/java/com/example/university/astroweathertwo/mainActivity/fragments/apiWeatherFragments/ForthcomingWeatherCondtionsFragment.java
+++ AstroWeatherTwo/app/src/main/java/com/example/university/astroweathertwo/mainActivity/fragments/apiWeatherFragments/ForthcomingWeatherCondtionsFragment.java
@@ -8,38 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import com.example.university.astroweathertwo.R;
 
 public class ForthcomingWeatherCondtionsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    public ForthcomingWeatherCondtionsFragment() {
-        // Required empty public constructor
-    }
+    // required empty constructor
+    public ForthcomingWeatherCondtionsFragment() { }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForthcomingWeatherCondtionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ForthcomingWeatherCondtionsFragment newInstance(String param1, String param2) {
         ForthcomingWeatherCondtionsFragment fragment = new ForthcomingWeatherCondtionsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -47,17 +31,24 @@ public class ForthcomingWeatherCondtionsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forthcoming_weather_condtions, container, false);
+        ScrollView rootView = (ScrollView)inflater.inflate(R.layout.fragment_forthcoming_weather_condtions, container, false);
+
+        LinearLayout linearLayout = (LinearLayout)rootView.findViewById(R.id.scroll_view_forthcomming_weather_linear_layout);
+
+        for(int i = 0 ; i < 10 ; i++) {
+            View view = inflater.inflate(R.layout.forthcoming_weather_tile,null);
+
+//            TextView textView = view.findViewById(R.id.forthcomming_temperature_text_view);
+
+            linearLayout.addView(view);
+        }
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -81,16 +72,6 @@ public class ForthcomingWeatherCondtionsFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
